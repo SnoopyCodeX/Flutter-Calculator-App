8999fb524ccd07396f541cd61c66fd7d1dbcca0c
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-03 10:14:40 +0800

    Refactored and added providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'calculator_page.dart';
+import 'package:provider/provider.dart';
+
+import './calculator_page.dart';
+import './result_provider.dart';
 
 void main() => runApp(CalculatorApp());
 
@@ -8,10 +11,13 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      home: CalculatorPage(),
+    return ChangeNotifierProvider(
+      create: (_) => ResultProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        home: CalculatorPage(),
+      ),
     );
   }
 }
