57363d0ad32f3fa98f0e327a13aa19ce97d1d9fa
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-05 06:29:24 +0800

    Refactored calculator_page.dart

--- lib/screens/calculator_page.dart
+++ lib/screens/calculator_page.dart
@@ -1,4 +1,6 @@
-import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:flutter/material.dart';
+import 'package:giffy_dialog/giffy_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../backend/constants.dart';
 import 'advanced/advanced_page.dart';
@@ -30,34 +32,68 @@ class _CalculatorPage extends State<CalculatorPage> with SingleTickerProviderSta
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFEDEBEC),
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.info_outlined,
-              color: kBlackColorText,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFEDEBEC),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(
+              onPressed: () {
+                showDialog(context: context, builder: (context) => _buildAboutDialog());
+              },
+              icon: Icon(
+                Icons.info_outlined,
+                color: kBlackColorText,
+              ),
             ),
-          ),
-        ],
-        title: TabBar(
-          indicatorColor: kOrangeColorText,
-          labelColor: kBlackColorText,
-          controller: tabController,
-          tabs: <Widget>[
-            Tab(text: tabs[0]),
-            Tab(text: tabs[1]),
           ],
-          onTap: (index) {
-            setState(() => currentIndex = index);
-          },
+          title: TabBar(
+            indicatorColor: kOrangeColorText,
+            labelColor: kBlackColorText,
+            controller: tabController,
+            tabs: <Widget>[
+              Tab(text: tabs[0]),
+              Tab(text: tabs[1]),
+            ],
+            onTap: (index) {
+              setState(() => currentIndex = index);
+            },
+          ),
         ),
+        body: currentIndex == 0 ? const BasicCalculatorPage() : const AdvancedCalculatorPage(),
       ),
-      body: currentIndex == 0 ? const BasicCalculatorPage() : const AdvancedCalculatorPage(),
+    );
+  }
+
+  Widget _buildAboutDialog() {
+    return GiffyDialog.lottie(
+      LottieBuilder.asset('assets/lottie/lottie-1.json'),
+      title: Text('About This App'),
+      content: Text(
+        'This is a simple calculator made with flutter. I made this 2 years ago as my first practice flutter application, and then decided to submit this as my entry for Studev\'s Quest #8 Calculator Challenge.'
+        ' I had to refactor and add some more features to this app because this only had basic functions when I first created this.\n\n'
+        'Collabed with: Leslie G. Martinez\n'
+        '© SnoopyCodeX 2021-${DateTime.now().year}',
+      ),
+      actions: [
+        TextButton(
+          onPressed: () async {
+            String githubRepoURL = "https://github.com/SnoopyCodeX/Flutter-Calculator-App";
+            if (await canLaunchUrl(Uri.parse(githubRepoURL))) {
+              await launchUrl(Uri.parse(githubRepoURL));
+            }
+
+            Navigator.of(context).pop();
+          },
+          child: Text('Open Github Repo'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text('Okay'),
+        ),
+      ],
     );
   }
 }
