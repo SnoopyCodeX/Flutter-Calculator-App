78675a731e96a82715f6052a18a8ae70d0977b3c
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-05 06:27:49 +0800

    Refactored main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_calculator_app/backend/valuex_provider.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:provider/provider.dart';
 
 import 'backend/result_provider.dart';
+import 'backend/valuex_provider.dart';
 import 'screens/calculator_page.dart';
 
 void main() {
@@ -14,15 +16,21 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    double height = MediaQuery.of(context).size.height;
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ResultProvider()),
         ChangeNotifierProvider(create: (_) => ValueXProvider()),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.light(),
-        home: CalculatorPage(),
+      child: FlutterWebFrame(
+        enabled: kIsWeb,
+        maximumSize: Size(496, height),
+        builder: (context) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.light(useMaterial3: true),
+          home: CalculatorPage(),
+        ),
       ),
     );
   }
