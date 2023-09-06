4503400740373958f87b7ce3634f39217ac6b000
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-06 11:15:18 +0800

    Updated constants.dart

--- lib/backend/constants.dart
+++ lib/backend/constants.dart
@@ -1,3 +1,6 @@
+/// Special thanks to my very special someone who pushed and convinced me to join the Studev's Challenge, Leslie G. Martinez
+///Copyright 2021-2023
+
 import 'package:flutter/material.dart';
 
 const kBlackColorText = Colors.black;
