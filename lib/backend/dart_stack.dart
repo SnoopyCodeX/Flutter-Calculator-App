8381ea8236cd7c6a0f346b522d65120b9f54395c
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-06 11:15:31 +0800

    Updated dart_stack.dart

--- lib/backend/dart_stack.dart
+++ lib/backend/dart_stack.dart
@@ -4,6 +4,10 @@
 ///
 /// Named "DartStack" because flutter already
 /// has a widget named "Stack".
+
+/// Special thanks to my very special someone who pushed and convinced me to join the Studev's Challenge, Leslie G. Martinez
+///Copyright 2021-2023
+
 class DartStack<E> {
   List _entries = [];
 
